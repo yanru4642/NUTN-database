491d9aa18319f062dad12d9ebc94ec54e34478b3
Alan Chen <yanru4642@gmail.com>
2024-01-05 11:55:41 +0800

    modified shipment

--- shipment/header.js
+++ shipment/header.js
@@ -1,5 +1,5 @@
 var header="";
-header += "<nav class=\"navbar navbar-expand-lg navbar-dark \" style=\"background-color: rgb(14, 46, 100);\">";
+header += "<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">";
 header += "      <div class=\"container-fluid\">";
 header += "          <a class=\"navbar-brand \" href=\"index.html\">啟勝電機行<\/a>";
 header += "          <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\">";
